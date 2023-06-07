42dcdc4073c17f9e8d023628739a3fb527883942
Stacy Brock <brock@oregonstate.edu>
2023-06-06 21:41:59 -0700

    Update formula for 9.0.1600

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v9.0.1500.tar.gz"
-  sha256 "4a473e8e387a0f947b7e27db4978c162f7ddc03273e080206b5adebd2ef9355e"
+  url "https://github.com/vim/vim/archive/v9.0.1600.tar.gz"
+  sha256 "6c95ac57bcf1e3caf36e304380cec50074705cfd4f104824b01dec3c1727cdce"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
@@ -17,13 +17,13 @@ class Vim < Formula
   end
 
   bottle do
-    sha256 arm64_ventura:  "7c279b8389a689a3bf0fc76b53862d5c3ab095bf7dc458af4d2b3d570a7078bd"
-    sha256 arm64_monterey: "2bfaf0ea729a17e8715f4c0dcb01abc2603769d8b3fc302b5bdb65c7f125eb04"
-    sha256 arm64_big_sur:  "21649981ec5214fe482f41c45025f4c321a0c8e114f2467600cdc5eb6579a6fe"
-    sha256 ventura:        "a04a7da191c8734b42afee527828780b32f5fb713a54b7073fd2f51aadc52a17"
-    sha256 monterey:       "834d56b40070c1af95794090c4b51ed0a331a6817fee8e8693e04ccadf0fca40"
-    sha256 big_sur:        "5f78c5a0af212e4ce89acf70a4108c14e0a855dd7c15afe0a4af922b926b1d47"
-    sha256 x86_64_linux:   "c710a795b59a702646a141bcb976948f4038f3d82b653b26a8532f0056162946"
+    sha256 arm64_ventura:  "07beb112011305152955513b4cb4531b9fe0b159ce37e097edeac01aac363cf6"
+    sha256 arm64_monterey: "bc05168f2b39fefdcbc9da3b959eae69be656e156a62545bb56dd0718fc226c0"
+    sha256 arm64_big_sur:  "8786112ac802d6f2fa33924fe78fa8e8c5465f6853b1acb5dae1fd54f1fdeaab"
+    sha256 ventura:        "3af92bd148510efe6ed371f9b3a8ae62eef255ae8902c4ee893fb0a51479c02a"
+    sha256 monterey:       "f32110f76c8a6b551a4b1db67c6a7218f1fda3198de3826545ca12b7f7951b46"
+    sha256 big_sur:        "a9cdec68be07ad328e32d8eb20b058a0207fb725871ae0b14201a99f6eb93b54"
+    sha256 x86_64_linux:   "b1be8471e32401d09c80ea78eed7d64bea3b762f8144ef33fa8b2f56da55d562"
   end
 
   depends_on "gettext"
