12f6746b4378e81c48a3eff03855b39d126b727a
Stacy Brock <kalrnux@gmail.com>
2023-11-01 21:35:33 -0700

    Add slimdown.sh script to up the laziness quotient

---
+++ slimdown.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/v/vim.rb -O Formula/vim.rb
+
+sed -i '' \
+    -e '/lua/d' \
+    -e '/perl/d' \
+    -e '/depends_on "ruby"/d' \
+    -e '/--enable-rubyinterp/d' \
+    -e 's/"--without-x",/"--without-x"/' \
+    Formula/vim.rb
