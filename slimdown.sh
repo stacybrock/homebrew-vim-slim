f6d9f1c3cd1ed9f1961dc9b1e606f29fbfa4610d
Stacy Brock <brock@oregonstate.edu>
2025-03-04 07:58:22 -0800

    Update slimdown.sh for new formulae name

--- slimdown.sh
+++ slimdown.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/v/vim.rb -O Formula/vim.rb
+wget https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/v/vim.rb -O Formula/vim-slim.rb
 
 sed -i '' \
+  -e 's/class Vim/class VimSlim/' \
   -e '/lua/d' \
   -e '/perl/d' \
   -e '/depends_on "ruby"/d' \
   -e '/--enable-rubyinterp/d' \
   -e 's/"--without-x",/"--without-x"/' \
-  Formula/vim.rb
+  Formula/vim-slim.rb
 
 # second pass of sed to clean up extraneous blank lines
 sed -i '' \
   -e '/^$/N;/^\n$/s/^\n$//;P;D' \
-  Formula/vim.rb
+  Formula/vim-slim.rb
