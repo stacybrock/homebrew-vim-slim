fd225fdf6f87c04ab5f00a06ecf46ca5fa5208b9
Stacy Brock <3768182+stacybrock@users.noreply.github.com>
2026-03-14 16:16:06 +0100

    Update formula for 9.2.0150

--- Formula/vim-slim.rb
+++ Formula/vim-slim.rb
@@ -2,8 +2,8 @@ class VimSlim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/refs/tags/v9.2.0100.tar.gz"
-  sha256 "0f35dbb9bcebc00d97b06aa229129f1a707960b64f064f65c7bcb8b424fe129b"
+  url "https://github.com/vim/vim/archive/refs/tags/v9.2.0150.tar.gz"
+  sha256 "bd56f5981eb4cb971eaf58725bf590f4b4c067b911b8e70dcd813b7de2392197"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
@@ -25,12 +25,12 @@ class VimSlim < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "812dc4a9975e50bafde8cd3c0080b50bb7d93da16910d33c2675c70079e3ab27"
-    sha256 arm64_sequoia: "e92bebfbfbfeaa95dfdc228934239e046e6232e80f7a58512ff704a35b923817"
-    sha256 arm64_sonoma:  "39f690061d9214032288afff94770093a63253b2c2decf0cfcf42eb997e8abf8"
-    sha256 sonoma:        "ff4d113744d871e57265ff925c414e4863e0973ea54355725d7b1e5b2271c349"
-    sha256 arm64_linux:   "5d20745184617e98fc6f328f9e951857eeee49b03e75e9ccb402dd269429db36"
-    sha256 x86_64_linux:  "249c5b8e98e704f12d2c1ff7cd015299516da64cbf47075c7dda150a0eb96fee"
+    sha256 arm64_tahoe:   "0d27f1d93093b1a44a7a56c40765aca1bfde3a986ae063e52edadc4abd6c4a27"
+    sha256 arm64_sequoia: "a5dae39c776c23c8c07860be8ff4c0fee887c6f9435fc1d43d136698259f3278"
+    sha256 arm64_sonoma:  "393c7600207f67b18ec30225c21fe40579874fb979adcb272b4c64d99c135663"
+    sha256 sonoma:        "8137023ede6342a84b937f2535b68bfc7f791cd34a81485c4935fd588f868c89"
+    sha256 arm64_linux:   "ef3864e6287227d253f8aefc86dea92953beab3bfd03abc74222f4cf20f1cf58"
+    sha256 x86_64_linux:  "b85a79769d8a2fb137e322e1113a7fa1f6fd51aee46daab802fae1569d23b490"
   end
 
   depends_on "gettext" => :build
